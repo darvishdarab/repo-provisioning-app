f031b7ff352e5a247ffa9e4927c3cc8089337219
darvishdarab <mali_dd@yahoo.com>
2021-11-16 10:36:23 -0500

    Initial Commit

--- src/main/java/CONSTANT.java
+++ src/main/java/CONSTANT.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class CONSTANTS {
+public class CONSTANT {
+    final static String STAFF_TEAM_ID = System.getenv("STAFF_TEAM_ID");
+    final static String GITHUB_API_BASE_URL = "https://api.github.com/";
+    final static String ORG_NAME = System.getenv("ORG_NAME");
+    final static String GITHUB_API = System.getenv("GITHUB_API");
+    final static String REPO_BASE_NAME = System.getenv("REPO_BASE_NAME");
+    final static int PORT = Integer.parseInt(System.getenv("PORT"));
 }
