aa0f6a998e97e9353c527117eeee806b9b8f34d2
darvishdarab <mali_dd@yahoo.com>
2021-11-16 10:33:08 -0500

    Initial commit

---
+++ src/main/java/Github.java
@@ -0,0 +1,109 @@
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.nio.client.CloseableHttpAsyncClient;
+import org.apache.http.impl.nio.client.HttpAsyncClients;
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+public class Github {
+
+    public static void createRepo(String name) throws IOException, ExecutionException, InterruptedException {
+        String url = CONSTANT.GITHUB_API_BASE_URL + "orgs/" + CONSTANT.ORG_NAME + "/repos";
+
+        CloseableHttpAsyncClient client = HttpAsyncClients.createDefault();
+        client.start();
+        HttpPost httpPost = new HttpPost(url);
+        httpPost.setHeader("Accept", "application/json");
+        httpPost.setHeader("Content-type", "application/json");
+
+        // set repo name and private to true
+        String json = "{\"name\":\"" + name + "\", \"private\": true}";
+        StringEntity entity = new StringEntity(json);
+
+        httpPost.setEntity(entity);
+
+        httpPost.addHeader("Authorization", "token " + CONSTANT.GITHUB_API );
+
+        Future<HttpResponse> future = client.execute(httpPost, null);
+        // and wait until a response is received
+        HttpResponse response = future.get();
+        client.close();
+
+        System.out.println("--------RESPONSE OF CREATING REPO------");
+        System.out.println();
+        System.out.println(response.toString());
+        System.out.println();
+        System.out.println("----END OF RESPONSE OF CREATING REPO---");
+
+    }
+    public static void addMembers(String repoName, String user) throws IOException, ExecutionException, InterruptedException {
+        String url = CONSTANT.GITHUB_API_BASE_URL + "repos/" + CONSTANT.ORG_NAME + "/" + repoName + "/collaborators/" + user;
+        CloseableHttpAsyncClient client = HttpAsyncClients.createDefault();
+        client.start();
+
+
+        HttpPut httpPut = new HttpPut(url);
+        httpPut.setHeader("Accept", "application/json");
+        httpPut.setHeader("Content-type", "application/json");
+
+        String json = "{\"permission\":\"admin\"}";
+        StringEntity entity = new StringEntity(json);
+
+        httpPut.setEntity(entity);
+
+
+        httpPut.addHeader("Authorization", "token " + CONSTANT.GITHUB_API);
+
+
+        Future<HttpResponse> future = client.execute(httpPut, null);
+        // and wait until a response is received
+        HttpResponse response = future.get();
+        client.close();
+
+        System.out.println("--------RESPONSE OF ADDING A MEMBER------");
+        System.out.println();
+        System.out.println(response.toString());
+        System.out.println();
+        System.out.println("----END OF RESPONSE OF ADDING A MEMBER---");
+
+    }
+
+    public static void addTeam(String repoName, String teamID) throws IOException, ExecutionException, InterruptedException {
+        String url = CONSTANT.GITHUB_API_BASE_URL + "teams/" + teamID + "/repos/" + CONSTANT.ORG_NAME + "/" + repoName;
+
+        CloseableHttpAsyncClient client = HttpAsyncClients.createDefault();
+        client.start();
+
+
+        HttpPut httpPut = new HttpPut(url);
+        httpPut.setHeader("Accept", "application/json");
+        httpPut.setHeader("Content-type", "application/json");
+
+        String json = "{\"permission\":\"admin\"}";
+        StringEntity entity = new StringEntity(json);
+
+        httpPut.setEntity(entity);
+
+
+        httpPut.addHeader("Authorization", "token " + CONSTANT.GITHUB_API);
+
+
+        Future<HttpResponse> future = client.execute(httpPut, null);
+        // and wait until a response is received
+        HttpResponse response = future.get();
+        client.close();
+
+        System.out.println("--------RESPONSE OF TEAMS------");
+        System.out.println();
+        System.out.println(response.toString());
+        System.out.println();
+        System.out.println("----END OF RESPONSE OF TEAMS---");
+
+
+    }
+
+
+}
---
+++ src/main/java/Server.java
@@ -0,0 +1,66 @@
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import static spark.Spark.port;
+import static spark.Spark.post;
+import static spark.Spark.get;
+
+public class Server {
+
+    final static int MAX_NUM_OF_PEOPLE = 20;
+
+
+    public static void main(String[] args) {
+
+        port(CONSTANT.PORT);
+
+        //homepage
+        get("/", (req, res) -> {
+            res.type("text/html; charset=UTF-8");
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "public/index.html");
+        }, new VelocityTemplateEngine() );
+
+        // create repo and add members
+        post("/", (req, res) -> {
+            List<String> firstNames = new ArrayList<>();
+            List<String> lastNames = new ArrayList<>();
+            List<String> jheds = new ArrayList<>();
+            List<String> githubUsers = new ArrayList<>();
+
+            for (int i = 1; i < MAX_NUM_OF_PEOPLE; ++i) {
+                if (req.queryParams("firstName" + String.valueOf(i)) != null)
+                    firstNames.add(req.queryParams("firstName" + String.valueOf(i)));
+                if (req.queryParams("lastName" + String.valueOf(i)) != null)
+                    lastNames.add(req.queryParams("lastName" + String.valueOf(i)));
+                if (req.queryParams("jhed" + String.valueOf(i)) != null)
+                    jheds.add(req.queryParams("jhed" + String.valueOf(i)));
+                if (req.queryParams("githubUser" + String.valueOf(i)) != null)
+                    githubUsers.add(req.queryParams("githubUser" + String.valueOf(i)));
+            }
+
+            String repoName = CONSTANT.REPO_BASE_NAME;
+            for (String jhed: jheds)
+                repoName += "-" + jhed;
+            Github.createRepo(repoName);
+            for (String user: githubUsers)
+                Github.addMembers(repoName, user);
+            Github.addTeam(repoName, CONSTANT.STAFF_TEAM_ID);
+
+            res.redirect("ty");
+            return "";
+        });
+
+        // page that gets displayed after from submission
+        get("/ty", (req, res) -> {
+            res.type("text/html; charset=UTF-8");
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "public/ty.html");
+        }, new VelocityTemplateEngine() );
+    }
+
+}
